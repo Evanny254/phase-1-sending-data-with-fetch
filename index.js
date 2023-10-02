9831dd45d1fdcfaf52a357aaabf703e655c8357d
Evanny254 <evancherry254@gmail.com>
2023-10-02 16:59:46 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+let userName = "Evans";
+let userEmail = "evans@evans.com";
+let body = document.querySelector("body");
+
+const submitData = (userName, userEmail) => {
+    let userObj = {name: userName, email: userEmail}
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userObj)
+  })
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    body.innerHTML = `<p>${data.id}</p>`;
+  })
+  .catch((error) => {
+    body.innerHTML = `${error.message}`;
+  });
+};
